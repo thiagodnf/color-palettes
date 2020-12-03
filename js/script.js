7c276e815ce45f93bc0f205e53f0d6eea15313b0
Thiago Ferreira <thiagodnf@users.noreply.github.com>
2020-12-03 12:22:12 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,6 +10,10 @@ var colorPalettes = [
     {
         version: "Highcharts 2.x",
         colors: ['#4572A7', '#AA4643', '#89A54E', '#80699B', '#3D96AE', '#DB843D', '#92A8CD', '#A47D7C', '#B5CA92']
+    },
+    {
+        version: "Random",
+        colors: ['#EAEFF2', '#AA4643', '#89A54E', '#80699B', '#3D96AE', '#DB843D', '#92A8CD', '#A47D7C', '#B5CA92']
     }
 ]
 
