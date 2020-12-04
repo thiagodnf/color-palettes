a88861085e430d35affe286e6e87831aaf15ebbc
Thiago Nascimento <thiagodnf@gmail.com>
2020-12-04 10:25:31 -0500

    added favicon

--- src/components/Head.js
+++ src/components/Head.js
@@ -15,6 +15,11 @@ function Component(props) {
             <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
             <meta name="apple-mobile-web-app-title" content="Color Palettes"/>
             <meta name="robots" content="index, follow"/>
+
+            <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png"/>
+            <link rel="manifest" href="/images/favicon/site.webmanifest"/>
         </Head>
     );
 }
