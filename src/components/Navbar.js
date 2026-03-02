f9844f365c9bf73d2ef5b5fa4e9ce2095daf7979
Thiago Ferreira <thiagodnf@users.noreply.github.com>
2026-03-02 11:15:29 -0500

    added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,12 @@ export default function Component() {
     return (
         <Navbar fixed="top" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#" className="gradient-text">
-                    Color Palettes
+                <Navbar.Brand href="#" className="gradient-text2">
+                    <img
+                        src="/color-palettes/images/logo-full-dark.png"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />
                 </Navbar.Brand>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto"></Nav>
